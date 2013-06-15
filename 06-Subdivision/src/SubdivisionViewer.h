20a27bdb319977695a356febb06d4a952c9c20e6
Doron Ben Elazar <b.e.doron@gmail.com>
2013-06-15 11:41:08 +0300

    Skeleton

---
+++ 06-Subdivision/src/SubdivisionViewer.h
@@ -0,0 +1,64 @@
+//=============================================================================
+//
+//  CLASS ParamViewer
+//
+//=============================================================================
+
+
+#ifndef SUBDIVISION_VIEWER_HH
+#define SUBDIVISION_VIEWER_HH
+
+
+//== INCLUDES =================================================================
+
+#include "MeshViewer.hh"
+#include <gmm.h>
+
+//== CLASS DEFINITION =========================================================
+
+	      
+
+class SubdivisionViewer : public MeshViewer
+{
+public:
+  
+
+   
+  /// default constructor
+  SubdivisionViewer(const char* _title, int _width, int _height);
+
+  // destructor
+  ~SubdivisionViewer();
+
+  /// open mesh
+  virtual bool open_mesh(const char* _meshfilename);
+
+
+private:
+  
+  virtual void init();
+  virtual void draw(const std::string& _draw_mode);
+
+  virtual void keyboard(int key, int x, int y);
+
+  void Perform_CatmullClark();
+  void Perform_Loop();
+
+private:
+  
+  //OpenMesh::VPropHandleT<OpenMesh::Vec2f>   vparam_u_, vparam_h_, texcoord_u_, texcoord_h_  ;
+  OpenMesh::VPropHandleT<Mesh::Point>       vpos_;
+  OpenMesh::FPropHandleT<Mesh::Point>       fcentroid_;
+  OpenMesh::EPropHandleT<Mesh::Point>       enewpoint_;
+  OpenMesh::EPropHandleT<VertexHandle>      enewvertex_;
+  //OpenMesh::EPropHandleT<Mesh::Scalar>      eweight_;
+
+  Mesh::Point _bbMin3D, _bbMax3D;//, _bbMin2D, _bbMax2D;
+
+};
+
+
+//=============================================================================
+#endif // SUBDIVISION_VIEWER_HH defined
+//=============================================================================
+
---
+++ 06-Subdivision/src/subdivider.cc
@@ -0,0 +1,13 @@
+//=============================================================================
+#include "SubdivisionViewer.h"
+
+
+
+int main(int argc, char **argv)
+{
+  glutInit(&argc, argv);
+
+  SubdivisionViewer window("Subdivision", 512, 512);
+
+  glutMainLoop();
+}
